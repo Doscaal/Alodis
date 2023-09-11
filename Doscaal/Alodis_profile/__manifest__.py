1eb1148e6547170c38183b1b5c633fb2d9a23f41
Alexandre Moreau <alexandre.moreau@doscaal.fr>
2023-09-11 10:18:22 +0200

    improve version

--- Doscaal/Alodis_profile/__manifest__.py
+++ Doscaal/Alodis_profile/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'Alodis Profile',
-    'version': '14.0.1.0',
+    'version': '14.0.1.1',
     'category': 'Custom',
     'description': """Profile for Alodis""",
     'author': 'Doscaal',

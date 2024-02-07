df1809af5f5a403b406ecbf3e60098772882713f
Alexandre Moreau <alexandre.moreau@doscaal.fr>
2024-02-07 17:07:39 +0100

    recalcul des champs pour mettre 0 sir devis

--- Doscaal/Alodis_profile/models/loyalty_card.py
+++ Doscaal/Alodis_profile/models/loyalty_card.py
@@ -6,8 +6,24 @@ from odoo import models, fields
 class LoyaltyCard(models.Model):
     _inherit = 'loyalty.card'
 
-    amount_total = fields.Monetary(related="order_id.amount_total")
-    amount_untaxed = fields.Monetary(related="order_id.amount_untaxed")
+    amount_total = fields.Monetary(string="Total TTC", compute="compute_ttc")
+    amount_untaxed = fields.Monetary(string="Total HT", compute="compute_ht")
     state_order = fields.Selection(related="order_id.state")
-    date_order = fields.Datetime(related="order_id.date_order")
+    date_order = fields.Datetime(string="Date de commande",
+                                 compute="compute_date")
     currency_id = fields.Many2one(related="order_id.currency_id")
+
+    def compute_ttc(self):
+        for rec in self:
+            rec.amount_total = rec.state in (
+                'sale', 'done') and rec.amount_total or 0
+
+    def compute_ht(self):
+        for rec in self:
+            rec.amount_untaxed = rec.state in (
+                'sale', 'done') and rec.amount_untaxed or 0
+
+    def compute_date(self):
+        for rec in self:
+            rec.date_order = rec.state in (
+                'sale', 'done') and rec.date_order or False

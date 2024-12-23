b5119e621c679b61fe329bb4a2d6a0d93a55cd97
Alexandre Moreau <alexandre.moreau@doscaal.fr>
2024-12-23 20:02:12 +0100

    identification erreur 2

--- Doscaal/Alodis_profile/models/__init__.py
+++ Doscaal/Alodis_profile/models/__init__.py
@@ -2,7 +2,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 # -*- coding: utf-8 -*-
 
-from . import loyalty_card
+# from . import loyalty_card
 # from . import sale_order
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
